9aa6441d1c556318b72b5476394a9debf34ccdf3
alejodiazzz <diaza.alejandro123@gmail.com>
2026-05-16 00:03:48 -0500

    debug: Agregar /ping y manual redirect en middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export default NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnAdmin = req.nextUrl.pathname.startsWith("/admin");
+
+  if (isOnAdmin && !isLoggedIn) {
+    // Forzamos la URL de redirección usando el host de la petición actual
+    const loginUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(loginUrl);
+  }
+  
+  return NextResponse.next();
+});
 
 export const config = {
-  // Coincidir con todas las rutas excepto las que empiezan con /api, /_next, o archivos estáticos
   matcher: ["/admin/:path*"],
 };
