c7ab00bc5a5db23dddfcd92883fad9acd2fa4c71
alejodiazzz <diaza.alejandro123@gmail.com>
2026-05-15 23:23:14 -0500

    debug: Eliminar middleware para descartar errores de ruteo

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { auth as middleware } from "@/auth";
-
-export const config = {
-  // Coincidir con todas las rutas excepto las que empiezan con /api, /_next, o archivos estáticos
-  matcher: ["/admin/:path*"],
-};
